3de8f0f7cd9fc5bac1e54732f7db2e98a062d7ec
jeremainecheong <132808191+jeremainecheong@users.noreply.github.com>
2026-04-21 16:32:35 +0800

    test(raft): killing a non-leader leaves the cluster untouched

--- integration-tests/src/test/java/jbroker/it/ThreeNodeRaftIT.java
+++ integration-tests/src/test/java/jbroker/it/ThreeNodeRaftIT.java
@@ -15,4 +15,20 @@ class ThreeNodeRaftIT {
             assertThat(leader.id().value()).isBetween(1, 3);
         }
     }
+
+    @Test
+    void killingNonLeaderDoesNotDisruptCluster(@TempDir Path dir) throws Exception {
+        try (var cluster = ClusterHarness.start(dir, 3)) {
+            var leader = cluster.waitForLeader(1_000);
+            var victim = cluster.nodes().stream()
+                    .filter(n -> !n.id().equals(leader.id()))
+                    .findFirst()
+                    .orElseThrow();
+            cluster.killNode(victim.id());
+
+            Thread.sleep(1_500);
+
+            assertThat(leader.driver().role()).isEqualTo(jbroker.raft.core.Role.LEADER);
+        }
+    }
 }
